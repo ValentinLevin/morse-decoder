444e05ab944b41890ef2dd21484fcda852d3dea8
Valentin Levin <levin_valentin@mail.ru>
2024-08-22 18:30:57 +0500

    feat: Реализована функция декодирования сообщения из зашифрованного сообщения в азбуке Морзе

--- src/index.js
+++ src/index.js
@@ -37,8 +37,44 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function decodeChar(encodedChar) {
+    if (encodedChar !== '**********') {
+        const values = {
+            '00': '',
+            '10': '.',
+            '11': '-'
+        };
+
+        const partLength = 2;
+        const partCount = Math.floor(encodedChar.length / partLength);
+
+        let result = '';
+
+        for (let partIndex = 0; partIndex < partCount; partIndex++) {
+            let encodedPart = encodedChar.slice(partIndex * partLength, (partIndex + 1) * partLength);
+            let partValue = values[encodedPart];
+            result = result + partValue;
+        }
+
+        return MORSE_TABLE[result];
+    } else {
+        return ' ';
+    }
+}
+
 function decode(expr) {
-    // write your solution here
+    const charCodeLength = 10;
+
+    let result = "";
+    let charCount = Math.floor(expr.length / charCodeLength);
+
+    for (let charIndex = 0; charIndex < charCount; charIndex++) {
+        let encodedChar = expr.slice(charIndex * charCodeLength, (charIndex+1) * charCodeLength);
+        let char = decodeChar(encodedChar);
+        result = result + char;
+    }
+
+    return result;
 }
 
 module.exports = {
